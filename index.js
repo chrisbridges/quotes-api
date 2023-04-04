855f30b108ccd0ed7c5fca14a7b2cf8e03c7d2f4
Chris Bridges <christopher_bridges@icloud.com>
2023-04-03 21:58:57 -0500

    basic server func

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server');
+// import schema from '.schema.graphql'
+const quotes = require('./quotes.json')
+
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields: 'title' and 'author'.
+  type Quote {
+    quote: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    randomQuote: String
+  }
+`;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello, world!'
+    randomQuote: () => quotes[Math.floor(Math.random()*quotes.length)].quote
   }
 };
 
