8db4e2539ac909dc00ec72b85f89190b212cf0be
Chris Bridges <christopher_bridges@icloud.com>
2024-04-02 17:43:04 -0500

    removing irrelevant comments

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ const quotes = require("./quotes.json");
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields: 'title' and 'author'.
   type Quote {
     quote: String
     author: String
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     randomQuote: String
   }
